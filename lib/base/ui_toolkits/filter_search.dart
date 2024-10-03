160879a55f7b4e8e3fa245f09e1b64c9db43dbb0
Sujal Shakya <sujalshakya387@gmail.com>
2024-10-03 12:17:02 +0545

    Fix: Minor UI changes of searchpage

--- lib/base/ui_toolkits/filter_search.dart
+++ lib/base/ui_toolkits/filter_search.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:search_page/search_page.dart';
 import 'package:thuprai_delivery/base/model/order_model.dart';
+import 'package:thuprai_delivery/base/ui_toolkits/label_text.dart';
 import 'package:thuprai_delivery/base/ui_toolkits/order.dart';
 
 SearchPage<Order> search(viewModel, BuildContext context, bool? orderDispatch) {
   return SearchPage<Order>(
+      barTheme: ThemeData(
+          colorScheme: const ColorScheme(
+              brightness: Brightness.light,
+              primary: Colors.black,
+              onPrimary: Color(0xff0066FF),
+              secondary: Colors.green,
+              onSecondary: Colors.grey,
+              error: Colors.red,
+              onError: Colors.white,
+              surface: Colors.white,
+              onSurface: Colors.black),
+          inputDecorationTheme: InputDecorationTheme(
+              hintStyle:
+                  TextStyle(color: Theme.of(context).colorScheme.onPrimary)),
+          appBarTheme: AppBarTheme(
+              iconTheme: IconThemeData(
+                  color: Theme.of(context).colorScheme.onPrimary))),
       items: viewModel.orders,
       searchLabel: 'Search orders',
       searchStyle: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
       suggestion: const Center(
-        child: Text('Filter orders by name, address and id'),
+        child: PrimaryText(text: 'Filter orders by name, address or id'),
       ),
       failure: const Center(
-        child: Text('No order found'),
+        child: PrimaryText(text: 'No order found'),
       ),
       filter: (order) => [
             order.number,
